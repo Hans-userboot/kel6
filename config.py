7865d05a6598abd2c5da409e852f6c7636627a07
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2024-09-26 21:12:15 +0700

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://kel5:kel6@cluster0.8kc80k
 
 RESTRICT = strtobool(os.environ.get("RESTRICT", "True"))
 
-FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "-1002157598685"))
+FORCE_SUB_1 = int(os.environ.get("FORCE_SUB_1", "-1002402596876"))
 FORCE_SUB_2 = int(os.environ.get("FORCE_SUB_2", "0"))
 FORCE_SUB_3 = int(os.environ.get("FORCE_SUB_3", "0"))
 FORCE_SUB_4 = int(os.environ.get("FORCE_SUB_4", "0"))
